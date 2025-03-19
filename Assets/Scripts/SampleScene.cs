5d44615f566d6faaaee69d7f86da47305d42c04b
nghiavt <nghiavt@sandinh.net>
2025-03-19 18:00:32 +0700

    Update project structure

---
+++ Assets/Scripts/SampleScene.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SampleScene : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    public void Update()
+    {
+        
+    }
+
+    public void OnPlayBtnClick() 
+    {
+        Debug.Log("Play button clicked");
+    }
+}
